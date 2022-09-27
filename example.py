a95cbe272aa56dc6f5e58938e4df9027495b6fe4
David Bichara <70610284+davidbichara@users.noreply.github.com>
2022-09-27 19:33:23 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,3 @@
-"""
-Demonstration of the GazeTracking library.
-Check the README.md for complete documentation.
-"""
 
 import cv2
 from gaze_tracking import GazeTracking
@@ -16,10 +12,8 @@ end = 0
 
 
 while True:
-    # We get a new frame from the webcam
     _, frame = webcam.read()
 
-    # We send this frame to GazeTracking to analyze it
     gaze.refresh(frame)
 
     frame = gaze.annotated_frame()
